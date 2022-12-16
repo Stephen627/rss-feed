d50491e67b449877fd46f7cb99c04cc9a3e09e0b
Stephen James <me@stephenjames.dev>
2022-12-16 11:38:40 +0000

    Add wrapped input

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text } from 'react-native'
+import Input from './src/components/Input'
 import BottomNav from './src/layout/BottomNav'
 import mainStyles from './src/styles'
 import { theme } from './src/theme'
@@ -7,6 +8,9 @@ import { theme } from './src/theme'
 export default function App() {
   return <BottomNav>
     <Text style={mainStyles.text}>Open up App.tsx to start working on your app!</Text>
+    <Input
+      value={'https://feed.laravel-news.com/'}
+    />
     <StatusBar
       animated={true}
       backgroundColor={theme.darkPrimary}
@@ -14,4 +18,3 @@ export default function App() {
     />
   </BottomNav>
 }
-
---
+++ src/components/Input.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent, useState } from 'react';
+import { TextInput, StyleSheet, TextInputProps } from 'react-native';
+import { theme } from '../theme';
+
+interface Props extends TextInputProps {
+}
+
+const Input: FunctionComponent<Props> = (props) => {
+  const [ focused, setFocused ] = useState<boolean>(false)
+
+  const style = { ...styles.input }
+  if (focused)
+    style.borderColor = theme.primary
+
+  return <TextInput
+    style={style}
+    onBlur={() => setFocused(false)}
+    onFocus={() => setFocused(true)}
+    {...props}
+  />
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+    color: theme.darkText,
+    borderColor: theme.darkSecondary,
+  }
+})
+
+export default Input
+

85d4e2b8323c61f32491af8d5d382ebdf686637c
Stephen James <me@stephenjames.dev>
2022-12-16 08:45:29 +0000

    Add theme colors

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+
+export const theme = {
+  primary: '#89B4FA',
+  secondary: '#B4BEFE',
+  lightText: '#313244',
+  darkText: '#CDD6F4',
+  darkPrimary: '#1E1E2E',
+  darkSecondary: 'rgb(42, 42, 62)',
+}
